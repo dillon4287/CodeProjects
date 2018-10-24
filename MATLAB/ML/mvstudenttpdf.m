b60d944284a3217da38eff7cf512d15da796ae4e
Dillon <dillonflann@gmail.com>
2018-10-24 16:30:36 -0700

    kow

--- MATLAB/ML/mvstudenttpdf.m
+++ MATLAB/ML/mvstudenttpdf.m
@@ -1,11 +1,11 @@
-function [ pdfval ] = MVstudenttpdf( X,mu, Scale,nu )
+function [ pdfval ] = mvstudenttpdf( X,mu, Scale,nu )
 % Correct.
 p = size(Scale,1);
 nuphalf = .5*(nu + p);
 phalf = .5*p;
 demean = X-mu;
-C = gammaln(nuphalf) - gammaln(nu*.5) - log(nu^phalf) -...
-    log(pi^phalf) - .5*log(det(Scale));
+C = gammaln(nuphalf) - gammaln(nu*.5) - phalf*log(nu*pi)-...
+    .5*log(det(Scale));
 inner= ((demean'/Scale)*demean)/nu;
 pdfval = C - nuphalf*log(1 +inner );
 end
--- MATLAB/factor_models/mhStepTwoRestrictioins.m
+++ MATLAB/factor_models/mhStepTwoRestrictioins.m
@@ -1,13 +1,25 @@
 function [ ] = mhStepTwoRestrictioins( y, ObsModelGuess, ObsVariance, Sworld, Sregion, Scountry)
 ObsModelPriorMean = zeros(3,1); 
 ObsModelPriorCov = eye(3)*10;
+df = 15;
 a = [-Inf, 0,0];
 b = [Inf, Inf, Inf];
 dll = @(guess) kowll(y, 1, guess, Sworld, Sregion, Scountry, .1);
-[ObsModelMean, ObsModelHess] = bhhh(ObsModelGuess, dll, 3, .1, .5);
-Candidate = geweke91T(a,b,ObsModelMean,ObsModelHess, 15, 1);
-% Num = log(mvnpdf(Candidate, ObsModelPriorMean,ObsModelPriorCov))
+[ObsModelMean, ObsModelHess] = bhhh(ObsModelGuess, dll, 3, .1, .5)
+dhess = diag(ObsModelHess);
+Candidate = geweke91T(a,b,ObsModelMean,diag(dhess), df, 1);
 
-kowLogLike(y, Candidate, ObsVariance, Sworld, Sregion, Scountry)
+Num = log(mvnpdf(Candidate, ObsModelPriorMean,ObsModelPriorCov)) + ...
+    kowLogLike(y, Candidate, ObsVariance, Sworld, Sregion, Scountry) +...
+    log(ttpdf(a(1), b(1), ObsModelMean(1), dhess(1), df, Candidate(1))) + ...
+     log(ttpdf(a(2), b(2), ObsModelMean(2), dhess(2), df, Candidate(2))) + ...
+     log(ttpdf(a(3), b(3), ObsModelMean(3), dhess(3), df, Candidate(3)));
+Den = log(mvnpdf(ObsModelGuess, ObsModelPriorMean,ObsModelPriorCov)) + ...
+    kowLogLike(y, Candidate, ObsVariance, Sworld, Sregion, Scountry) +...
+    log(ttpdf(a(1), b(1), ObsModelMean(1), dhess(1), df, Candidate(1))) + ...
+     log(ttpdf(a(2), b(2), ObsModelMean(2), dhess(2), df, Candidate(2))) + ...
+     log(ttpdf(a(3), b(3), ObsModelMean(3), dhess(3), df, Candidate(3)));
+
+
 end
 
