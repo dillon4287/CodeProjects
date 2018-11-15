da356c1365da339438dcdd54aa69ca6ee9ff173c
HPC <flannerd@uci.edu>
2018-11-15 15:42:02 -0800

    lin

--- MATLAB/factor_models/KOW/kowMcc.m
+++ MATLAB/factor_models/KOW/kowMcc.m
@@ -1,4 +1,13 @@
-function [] = kowMcc()
+function [] = kowMcc(Sims, burnin)
+if ischar(Sims)
+    Sims = str2num(Sims);
+end
+if ischar(burnin)
+    Sims = str2num(burnin);
+end
+
+
+
 [y, surx] = kowGenData();
 K = size(y,1);
 rng(1)
@@ -6,7 +15,7 @@ r0 = 10.*ones(K,1);
 v0 = 5;
 b0 = 1;
 B0 = 1;
-[f, f2 b, b2, v, v2] = kowdynfactorgibbs(y, surx,  b0, inv(B0), v0, r0, 5, 1)
+[f, f2 b, b2, v, v2] = kowdynfactorgibbs(y, surx,  b0, inv(B0), v0, r0, Sims, burnin)
 save('kowoutput.mat')
 
 end
