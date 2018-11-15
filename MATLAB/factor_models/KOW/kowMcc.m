b74bcbb543612453d8b2d05326c667ae989253b7
Dillon <dillonflann@gmail.com>
2018-11-15 15:41:24 -0800

    ubun

--- MATLAB/factor_models/KOW/kowMcc.m
+++ MATLAB/factor_models/KOW/kowMcc.m
@@ -1,5 +1,7 @@
 function [] = kowMcc()
-[y, surx] = kowGenData();
+% [y, surx] = kowGenData();
+load('surx.mat');
+load('kowy.mat');
 K = size(y,1);
 rng(1)
 r0 = 10.*ones(K,1);
@@ -7,7 +9,8 @@ v0 = 5;
 b0 = 1;
 B0 = 1;
 [f, f2 b, b2, v, v2] = kowdynfactorgibbs(y, surx,  b0, inv(B0), v0, r0, 5, 1)
-save('kowoutput.mat')
 
+fname = createDateString('kow_')
+save(fname)
 end
 
--- MATLAB/factor_models/kowmain.m
+++ MATLAB/factor_models/kowmain.m
@@ -11,6 +11,7 @@ r0 = 10.*ones(K,1);
 v0 = 5;
 b0 = 1;
 B0 = 1;
-[f, f2 b, b2, v, v2] = kowdynfactorgibbs(y, surx,  b0, inv(B0), v0, r0, 5, 1)
+[f, f2 b, b2, v, v2] = kowdynfactorgibbs(y, surx,  b0, inv(B0), v0,...
+    r0, 5, 1)
 
 
