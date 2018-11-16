d725401b6ed8e5c6b532399cd2caab25247be2db
Dillon <dillonflann@gmail.com>
2018-11-15 19:14:56 -0800

    linux

--- MATLAB/factor_models/KOW/kowupdateBetaPriors.m
+++ MATLAB/factor_models/KOW/kowupdateBetaPriors.m
@@ -3,12 +3,17 @@ function [ b, demeanedyt ] = kowupdateBetaPriors(ys, SurX, obsModelPrecision, St
 
 nEqns = length(obsModelPrecision);
 nFactors = size(StateObsModel,2);
-
+sizeStatePre = size(StatePrecision,1);
 i = 1:nEqns;
 j = 1:nFactors;
 fullpre = spdiags(obsModelPrecision,0, nEqns, nEqns);
-Pinverse = (StatePrecision + kron(speye(T),...
-    StateObsModel'* fullpre *StateObsModel))\speye(size(StatePrecision,1));
+Inside = StatePrecision + kron(speye(T),...
+    StateObsModel'* fullpre *StateObsModel);
+[L, p] = chol(Inside);
+Li = L\eye(sizeStatePre);
+Pinverse = Li*Li';
+% Pinverse = (StatePrecision + kron(speye(T),...
+%     StateObsModel'* fullpre *StateObsModel))\speye(sizeStatePre);
 Astar = StateObsModel'*fullpre;
 cx = size(SurX,2);
 addup = zeros(cx,cx);
--- MATLAB/factor_models/kowConditionalParametersT.m
+++
@@ -1,7 +0,0 @@
-function [condmean, condvar] = kowConditionalParametersT(mui, Hii, Hinoti, Hnotinoti, xnot, munot, df1, df2)
-demurestrict = xnot-munot;
-condmean = mui - Hii\Hinoti *(demurestrict);
-condvar = ((df1 + (demurestrict'*Hnotinoti*demurestrict))/ df2);
-
-end
-
