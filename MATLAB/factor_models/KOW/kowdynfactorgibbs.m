8fffd6ca5ca3f3a8c0b36550542f4358e552a5d3
dillonflannery-valadez <dillon4287@gmail.com>
2018-11-29 15:25:22 -0800

    all

--- MATLAB/factor_models/KOW/kowdynfactorgibbs.m
+++ MATLAB/factor_models/KOW/kowdynfactorgibbs.m
@@ -145,11 +145,11 @@ for i = 1 : Sims
         % Save a temporary object every 100 iterations after the burnrin
         if mod(i,100) == 0
             tempfilename = createDateString('tempFtupdate');
-            tempfilename = createDateString('tempFt2update');
+            tempfilename2 = createDateString('tempFt2update');
             tempitem = sumFt./(i-burnin);
             tempitem2 = sumFt2./(i-burnin);
             save(tempfilename, 'tempitem');
-            save(tempfileame2, 'tempitem2');
+            save(tempfilename2, 'tempitem2');
         end
     end
 end
