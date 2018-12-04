70fc654236f1f976ce5afa746a1958ee69fd3d98
dillonflannery-valadez <dillon4287@gmail.com>
2018-12-04 15:39:01 -0800

    me

--- MATLAB/factor_models/KOW/kow2Period.m
+++ MATLAB/factor_models/KOW/kow2Period.m
@@ -7,9 +7,9 @@ if ischar(bn)
     bn = str2num(bn);
 end
 
-[f, f2, b, v] = kowdynfactorgibbs(kowp1y, kowp1x,  v0, r0,  S, bn);
-[f, f2, b, v] = kowdynfactorgibbs(kowp2y, kowp2x,  v0, r0,  S, bn);
-[f, f2, b, v] = kowdynfactorgibbs(kowpapery, kowpaperx,  v0, r0,  S, bn);
+[f, f2, stf, b, v] = kowdynfactorgibbs(kowp1y, kowp1x,  v0, r0,  S, bn);
+[f, f2, stf, b, v] = kowdynfactorgibbs(kowp2y, kowp2x,  v0, r0,  S, bn);
+[f, f2, stf, b, v] = kowdynfactorgibbs(kowpapery, kowpaperx,  v0, r0,  S, bn);
 fname = createDateString('k2p_')
 save(fname)
 end
