e2920bddc47afc0be7d5e51731db13cd18aed8be
dillonflannery-valadez <dillon4287@gmail.com>
2018-12-04 13:07:53 -0800

    mes

--- MATLAB/factor_models/KOW/kow2Period.m
+++ MATLAB/factor_models/KOW/kow2Period.m
@@ -1,8 +1,15 @@
-function [  ] = kow2Period( )
+function [  ] = kow2Period( Sims, burnin)
 load('kow2p.mat')
-[f, f2, b, v] = kowdynfactorgibbs(kowp1y, kowp1x,  v0, r0,  2000, 200);
-[f, f2, b, v] = kowdynfactorgibbs(kowp2y, kowp2x,  v0, r0,  2000, 200);
-[f, f2, b, v] = kowdynfactorgibbs(kowpapery, kowpaperx,  v0, r0,  2000, 200);
+if ischar(Sims)
+    Sims = str2num(Sims);
+end
+if ischar(burnin)
+    burnin = str2num(burnin);
+end
+
+[f, f2, b, v] = kowdynfactorgibbs(kowp1y, kowp1x,  v0, r0,  S, bn);
+[f, f2, b, v] = kowdynfactorgibbs(kowp2y, kowp2x,  v0, r0,  S, bn);
+[f, f2, b, v] = kowdynfactorgibbs(kowpapery, kowpaperx,  v0, r0,  S, bn);
 fname = createDateString('k2p_')
 save(fname)
 end
