36dd301acb261172e67d287473cfd3ade0859b54
dillonflannery-valadez <dillon4287@gmail.com>
2018-12-10 11:21:02 -0800

    m

--- MATLAB/factor_models/KOW/logigampdf.m
+++ MATLAB/factor_models/KOW/logigampdf.m
@@ -1,7 +1,8 @@
 function [ logpdf ] = logigampdf( x,alpha,beta )
-constant = alpha*log(beta) - gammaln(alpha);
-nonexpterm = -(alpha+1)*log(x);
-logpdf = constant + nonexpterm - (beta/x);
+% Verified in R
+constant = alpha.*log(beta) - gammaln(alpha);
+nonexpterm = -(alpha+1).*log(x);
+logpdf = constant + nonexpterm - (beta./x);
 
 
 end
