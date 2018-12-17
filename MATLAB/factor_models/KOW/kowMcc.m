eb78897eae1969abd0d1c87a46ab3e8224473695
dillonflannery-valadez <dillon4287@gmail.com>
2018-12-17 15:50:36 -0800

    ,m

--- MATLAB/factor_models/KOW/kowMcc.m
+++ MATLAB/factor_models/KOW/kowMcc.m
@@ -1,4 +1,4 @@
-function [] = kowMcc(Sims, burnin)
+function [] = kowMcc(Sims, burnin, ReducedRuns)
 % [y, surx] = kowGenData();
 if ischar(Sims)
     Sims = str2num(Sims);
@@ -13,7 +13,7 @@ r0 = 10.*ones(K,1);
 v0 = 5;
 b0 = 1;
 B0 = 1;
-[f, f2, fmat, b, A, v] = kowdynfactorgibbs(kowy, kowx,  v0, r0, Sims, burnin)
+[f,f2,ml] = kowdynfactorgibbs(kowy, kowx,  v0, r0, Sims, burnin, ReducedRuns)
 
 fname = createDateString('kow_')
 save(fname)
