dcfe9768620faaedd1ac5b4b1fde3f7737bd2c5b
HPC <flannerd@uci.edu>
2019-02-12 20:21:06 -0800

    after delete

--- MATLAB/factor_models/MLFVAR/bashworld2.sh
+++ MATLAB/factor_models/MLFVAR/bashworld2.sh
@@ -1,10 +1,10 @@
 #!bin/bash
 #$ -S /bin/bash
 #$ -q its
-#$ -N sim100_2_2_200
-#$ -l mem_free=8G
+#$ -N world2_
+#$ -l mem_free=4G
 #$ -cwd
 #$ -notify 
 #$ -M dillonflann@gmail.com
 module load MATLAB
-./mfvar 10000 1000 1000 2 1 Sim100_2_2_200.mat
+./mfvar 10000 1000 1000 30 0 WorldData.mat
--- MATLAB/factor_models/MLFVAR/sim1.sh
+++ MATLAB/factor_models/MLFVAR/sim1.sh
@@ -1,10 +1,10 @@
 #!bin/bash
 #$ -S /bin/bash
 #$ -q its
-#$ -N sim10_10_200
+#$ -N sim1
 #$ -l mem_free=4G
 #$ -cwd
 #$ -notify 
 #$ -M dillonflann@gmail.com
 module load MATLAB
-./mfvar 10000 1000 1000 2 1 Sim10_10_200.mat
+./MFVARSim1
