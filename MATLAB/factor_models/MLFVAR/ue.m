77429699f5c3e20cb9431d9ad24e4b80a4857370
dillon4287 <dillonflann@gmail.com>
2019-05-28 15:01:48 -0700

    m

--- MATLAB/factor_models/MLFVAR/ue.m
+++ MATLAB/factor_models/MLFVAR/ue.m
@@ -21,8 +21,8 @@ v0=3;
 r0 =5;
 s0 = 3;
 d0 = 5;
-Sims = 10000;
-burnin = 2000;
+Sims = 3;
+burnin = 1;
 ReducedRuns = 10;
 initBeta = ones(dimX,1);
 obsPrecision = ones(K,1);
@@ -30,7 +30,7 @@ initStateTransitions = .3.*ones(nFactors,1);
 
 [Identities, sectorInfo, factorInfo] = MakeObsModelIdentity( InfoCell);
 
-initobsmodel = unifrnd(0,1,K,3);
+initobsmodel = unifrnd(0,1,K,levels);
 StateObsModel = makeStateObsModel(initobsmodel,Identities,0);
 vecFt  =  kowUpdateLatent(yt(:),  StateObsModel, ...
     kowStatePrecision(diag(initStateTransitions),ones(nFactors,1),T), obsPrecision);
@@ -44,7 +44,7 @@ t = 1:59;
 [sumFt, sumFt2, sumOM, sumOM2, sumST, sumST2,...
     sumBeta, sumBeta2, sumObsVariance, sumObsVariance2,...
     sumFactorVar, sumFactorVar2,sumVarianceDecomp,...
-   sumVarianceDecomp2] = MultDyFacVar(yt, Xt,  InfoCell, Sims,...
+   sumVarianceDecomp2] = Mldfvar(yt, Xt,  InfoCell, Sims,...
     burnin, ReducedRuns, initFactor, initBeta, initobsmodel,...
     initStateTransitions, v0, r0, s0, d0, identification, estML)
 
