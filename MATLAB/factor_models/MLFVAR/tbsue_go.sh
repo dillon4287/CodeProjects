3449ee491cb43cd550fad6b25227b1905c4c1e21
dillon4287 <dillonflann@gmail.com>
2019-06-10 20:58:51 -0700

    m

--- MATLAB/factor_models/MLFVAR/tbsue_go.sh
+++ MATLAB/factor_models/MLFVAR/tbsue_go.sh
@@ -1,14 +1,14 @@
 #!bin/bash
 #$ -S /bin/bash
 #$ -q its
-#$ -N tbsmpy_3
+#$ -N tbsue
 #$ -l mem_free=4G
 #$ -cwd
 #$ -notify 
 #$ -M dillonflann@gmail.com
 #$ -m ea
 module load MATLAB
-./tbs 2 1 1 ${KEY} mpy.mat
+./tbs 2 1 1 ${KEY} ue.mat
 
 
 
