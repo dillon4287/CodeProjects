3415ea5a13eaaeb5abeb9b4f88ef0a94daf9f94f
dillon4287 <dillonflann@gmail.com>
2019-06-18 13:26:22 -0700

    m

---
+++ MATLAB/factor_models/MLFVAR/tbs_sim.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env/ python
+import csv, subprocess, os
+files = os.listdir('/home/precision/CodeProjects/MATLAB/factor_models/MLFVAR/TimeBreakData/')
+print(len(files))
+for f in files:
+    qs = "qsub -v DATA={0} tbs_sim_study.sh".format(f)
+    print(qs)
+#   call =subprocess.call(qs, shell=True)
+  #  if call == 1:
+    #    print("failed")
