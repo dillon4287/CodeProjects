055ebde09b9d927e05c9f5f790017235faee5eaf
dillon4287 <dillonflann@gmail.com>
2019-09-25 10:54:40 -0700

    K

--- MATLAB/factor_models/MLFVAR/TimeBreakKow.m
+++ MATLAB/factor_models/MLFVAR/TimeBreakKow.m
@@ -27,15 +27,15 @@ v0=6;
 r0 =10;
 s0 = 6;
 d0 = 10;
-initBeta = ones(dimX,1);
+initBeta = .1.*ones(dimX,1);
 obsPrecision = ones(K,1);
-initStateTransitions = .3.*ones(nFactors,1);
+initStateTransitions = .1.*ones(nFactors,1);
 [Identities, sectorInfo, factorInfo] = MakeObsModelIdentity( InfoCell);
-initobsmodel = .01.*ones(K,levels);
+initobsmodel = .1.*ones(K,levels);
 StateObsModel = makeStateObsModel(initobsmodel,Identities,0);
-% vecFt  =  kowUpdateLatent(yt(:),  StateObsModel, ...
-%     kowStatePrecision(diag(initStateTransitions),ones(nFactors,1),T), obsPrecision);
-vecFt = .5.*ones(nFactors*T, 1);
+vecFt  =  kowUpdateLatent(yt(:),  StateObsModel, ...
+    kowStatePrecision(diag(initStateTransitions),ones(nFactors,1),T), obsPrecision);
+% vecFt = .5.*ones(nFactors*T, 1);
 initFactor = reshape(vecFt, nFactors,T);
 identification = 2;
 estML = 1;
