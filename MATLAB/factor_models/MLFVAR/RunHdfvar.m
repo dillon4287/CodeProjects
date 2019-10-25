cdd7a9e6a5f2ab552ab09a86ed374a56fedb56a0
dillon4287 <dillonflann@gmail.com>
2019-10-25 13:55:21 -0700

    M

---
+++ MATLAB/factor_models/MLFVAR/RunHdfvar.m
@@ -0,0 +1,70 @@
+function [] = RunHdfvar(Sims, burnin, DataLocation,DotMatFile, OutputDir)
+if ischar(Sims)
+    Sims = str2num(Sims);
+end
+if ischar(burnin)
+    burnin = str2num(burnin);
+end
+
+fprintf('Current Working Directory\n')
+pwd
+if (DataLocation == "")
+    load(DotMatFile)
+    disp(DotMatFile)
+else
+    datalocation = join([DataLocation,'/', DotMatFile]);
+    disp(DotMatFile)
+    load(datalocation, 'DataCell')
+end
+yt = DataCell{1,1};
+Xt = DataCell{1,2};
+InfoCell = DataCell{1,3};
+[K,T] = size(yt);
+[~, dimX] = size(Xt);
+levels = size(InfoCell,2);
+nFactors =  sum(cellfun(@(x)size(x,1), InfoCell));
+v0=6;
+r0 =10;
+s0 = 6;
+d0 = 10;
+obsPrecision = ones(K,1);
+initStateTransitions = .1.*ones(nFactors,1);
+[Identities, sectorInfo, factorInfo] = MakeObsModelIdentity( InfoCell);
+initobsmodel = .1.*ones(K,levels);
+StateObsModel = makeStateObsModel(initobsmodel,Identities,0);
+vecFt  =  kowUpdateLatent(yt(:),  StateObsModel, ...
+    kowStatePrecision(diag(initStateTransitions), ones(nFactors,1),T), obsPrecision);
+% vecFt = .5.*ones(nFactors*T, 1);
+initFactor = reshape(vecFt, nFactors,T);
+identification = 2;
+%%%%%%%%%%%%
+%%%%%%%%%%%%
+%DONT FORGET TO TURN THIS ON!!!!!!!!!
+%%%%%%%%%%%%
+%%%%%%%%%%%%
+estML = 1; %%%%%%
+%%%%%%%%%%%%
+%%%%%%%%%%%%
+[storeFt, storeBeta, storeOM, storeStateTransitions,...
+    storeObsPrecision, storeFactorVar,varianceDecomp, ml] = Mldfvar(yt, Xt,  InfoCell, Sims,...
+    burnin, initFactor,  initobsmodel,...
+    initStateTransitions, v0, r0, s0, d0, identification, estML, DotMatFile);
+period = strfind(DotMatFile, '.');
+
+dateCreated = createDateString('');
+fname = join(['Result_', DotMatFile(1:period-1)]);
+dirname = join(['~/CodeProjects/MATLAB/factor_models/MLFVAR/', OutputDir]);
+if ~exist(dirname, 'dir')
+    mkdir(dirname)
+end
+periodloc = strfind(DotMatFile, '.') ;
+if (OutputDir == "")
+    filename = join([dirname,fname, '_', dateCreated]);
+else
+    filename = join([dirname,'/',fname, '_', dateCreated]);
+end
+fprintf('Saving file %s \n', filename)
+save(filename)
+
+
+end
--- MATLAB/factor_models/MLFVAR/tbkowTESTS.m
+++ MATLAB/factor_models/MLFVAR/tbkowTESTS.m
@@ -1,6 +1,8 @@
 clear;clc;
+
 % DataCell = SimDataMLF(100, 1, 1,6, 1);
 % save('simdata', 'DataCell')
+% RunHdfvar(3, 1, '', 'simdata.mat', 'TestDir')
 % TimeBreakKow(200,  20, '', 'simdata.mat', 'TestDir')
 % TimeBreakKow(10,  2, 'BigKow', 'kowz.mat', 'TestDir')
 % TimeBreakKow(10, 4, 6, '', 'mpy.mat', 'TestDir')
@@ -18,45 +20,43 @@ clear;clc;
 % plot(up(1,:),   'r')
 % plot(down(1,:),   'r')
 % plot(sumft(1,:), 'black')
-
-
-DataCell = SimDataMLF(50, 1, 2,3, 1);
-Sims = 10;
-burnin = 5;
-estML = 1;
-yt = DataCell{1,1};
-Xt = DataCell{1,2};
-InfoCell = DataCell{1,3};
-Factor = DataCell{1,4};
-gamma = diag(DataCell{1,6});
-A = DataCell{1,7}
-[K,T] = size(yt);
-[~, dimX] = size(Xt);
-levels = size(InfoCell,2);
-nFactors =  sum(cellfun(@(x)size(x,1), InfoCell));
-v0=6;
-r0 =10;
-s0 = 6;
-d0 = 10;
-obsPrecision = ones(K,1);
-initStateTransitions = .1.*ones(nFactors,1);
-[Identities, sectorInfo, factorInfo] = MakeObsModelIdentity( InfoCell);
-initobsmodel = .1.*ones(K,levels);
-StateObsModel = makeStateObsModel(initobsmodel,Identities,0);
-vecFt  =  kowUpdateLatent(yt(:),  StateObsModel, ...
-    kowStatePrecision(diag(initStateTransitions), ones(nFactors,1),T), obsPrecision);
+% DataCell = SimDataMLF(50, 1, 2,3, 1);
+% Sims = 10;
+% burnin = 5;
+% estML = 1;
+% yt = DataCell{1,1};
+% Xt = DataCell{1,2};
+% InfoCell = DataCell{1,3};
+% Factor = DataCell{1,4};
+% gamma = diag(DataCell{1,6});
+% A = DataCell{1,7}
+% [K,T] = size(yt);
+% [~, dimX] = size(Xt);
+% levels = size(InfoCell,2);
+% nFactors =  sum(cellfun(@(x)size(x,1), InfoCell));
+% v0=6;
+% r0 =10;
+% s0 = 6;
+% d0 = 10;
+% obsPrecision = ones(K,1);
+% initStateTransitions = .1.*ones(nFactors,1);
+% [Identities, sectorInfo, factorInfo] = MakeObsModelIdentity( InfoCell);
+% initobsmodel = .1.*ones(K,levels);
+% StateObsModel = makeStateObsModel(initobsmodel,Identities,0);
+% vecFt  =  kowUpdateLatent(yt(:),  StateObsModel, ...
+%     kowStatePrecision(diag(initStateTransitions), ones(nFactors,1),T), obsPrecision);
 % vecFt = .5.*ones(nFactors*T, 1);
 % initFactor = reshape(vecFt, nFactors,T);
 
-initFactor = Factor;
-
-identification = 2;
-for u = 1:levels
-    BlockingInfo{u}  = InfoCell{levels};
-end
-
-
-
-[storeFt, storeBeta, storeOM, storeStateTransitions,...
-    storeObsPrecision, storeFactorVar,varianceDecomp, ml] = Hdfvar(yt, Xt,  InfoCell, BlockingInfo,...
-    Sims, burnin, initFactor,  A, gamma, v0, r0, s0, d0, identification, estML, '');
+% initFactor = Factor;
+% 
+% identification = 2;
+% for u = 1:levels
+%     BlockingInfo{u}  = InfoCell{levels};
+% end
+% 
+% 
+% 
+% [storeFt, storeBeta, storeOM, storeStateTransitions,...
+%     storeObsPrecision, storeFactorVar,varianceDecomp, ml] = Hdfvar(yt, Xt,  InfoCell, BlockingInfo,...
+%     Sims, burnin, initFactor,  A, gamma, v0, r0, s0, d0, identification, estML, '');
