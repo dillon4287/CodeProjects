e98afb52f52f4ac19a76dd8dea995787a4c469f1
dillon4287 <dillonflann@gmail.com>
2019-12-23 13:25:04 -0800

    Standard message.

--- MATLAB/factor_models/MLFVAR/initCovar.m
+++ MATLAB/factor_models/MLFVAR/initCovar.m
@@ -13,10 +13,11 @@ Im = speye(size(PhiKronPhi,1));
 R = full(spdiags(ones(K), 0, K*lags,K));
 RRT = R*R';
 P0 = (Im - PhiKronPhi)\RRT(:);
+P0 = reshape(P0, K*lags,K*lags);
 [~,p] = chol(P0);
 if p ~= 0 
     notvalid = 1;
 end
-P0 = reshape(P0, K*lags,K*lags);
+
 end
 
